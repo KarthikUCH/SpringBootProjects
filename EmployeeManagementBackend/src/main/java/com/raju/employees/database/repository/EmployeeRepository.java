af436670c05f8573e8428bcd30624de2d7fc833d
Karthi Keyan <karthikeyan027@gmail.com>
2021-05-15 11:47:10 +0530

    Create EmployeeManagementSystem project

---
+++ EmployeeManagementBackend/src/main/java/com/raju/employees/database/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.raju.employees.database.repository;
+
+import com.raju.employees.database.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
