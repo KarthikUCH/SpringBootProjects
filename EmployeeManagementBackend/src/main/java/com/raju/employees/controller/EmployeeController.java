3e849dd307ba4bffac6405f9f3e9d2e8671c24d8
Karthi Keyan <karthikeyan027@gmail.com>
2021-05-20 16:38:03 +0530

    Integrate get employee API

--- EmployeeManagementBackend/src/main/java/com/raju/employees/controller/EmployeeController.java
+++ EmployeeManagementBackend/src/main/java/com/raju/employees/controller/EmployeeController.java
@@ -2,7 +2,9 @@ package com.raju.employees.controller;
 
 import com.raju.employees.database.model.Employee;
 import com.raju.employees.database.repository.EmployeeRepository;
+import com.raju.employees.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,14 +17,24 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    // get all employees api
     @GetMapping("employees")
     public List<Employee> getAll(){
         return employeeRepository.findAll();
     }
 
+    // create new employee api
     @PostMapping("employees")
     public Employee saveEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    // get employee by id api
+    @GetMapping("employees/{id}")
+    public ResponseEntity<Employee>  getEmployee(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id "+id));
+        return ResponseEntity.ok(employee);
     }
 
 }
