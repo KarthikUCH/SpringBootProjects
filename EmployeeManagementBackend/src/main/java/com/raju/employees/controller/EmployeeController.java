aad36727610c475dfa666be8e8f605c12f456381
Karthi Keyan <karthikeyan027@gmail.com>
2021-05-20 16:51:09 +0530

    Integrate update employee API

--- EmployeeManagementBackend/src/main/java/com/raju/employees/controller/EmployeeController.java
+++ EmployeeManagementBackend/src/main/java/com/raju/employees/controller/EmployeeController.java
@@ -17,24 +17,37 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    // get all employees api
+    // get all employees rest api
     @GetMapping("employees")
     public List<Employee> getAll(){
         return employeeRepository.findAll();
     }
 
-    // create new employee api
+    // create new employee rest api
     @PostMapping("employees")
     public Employee saveEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
     }
 
-    // get employee by id api
+    // get employee by id rest api
     @GetMapping("employees/{id}")
-    public ResponseEntity<Employee>  getEmployee(@PathVariable Long id){
+    public ResponseEntity<Employee> getEmployee(@PathVariable Long id){
         Employee employee = employeeRepository.findById(id).
                 orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id "+id));
         return ResponseEntity.ok(employee);
     }
 
+    // update employee rest api
+
+    @PutMapping("employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetail){
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id "+id));
+        employee.setFirstName(employeeDetail.getFirstName());
+        employee.setLastName(employeeDetail.getLastName());
+        employee.setLastName(employeeDetail.getEmailId());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
 }
